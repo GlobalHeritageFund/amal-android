0fc90ad2b8d2a40cae8796590355b1dd5b3d819e
Soroush Khanlou <soroushkhanlou@gmail.com>
2017-11-05 17:08:50 -0500

    add the first bit of camera code

--- app/src/main/java/amal/global/amal/RootActivity.java
+++ app/src/main/java/amal/global/amal/RootActivity.java
@@ -3,6 +3,7 @@ package amal.global.amal;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.hardware.Camera;
 
 public class RootActivity extends AppCompatActivity {
 
@@ -11,6 +12,23 @@ public class RootActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_root);
 
+        if (this.getCameraInstance() != null) {
+            Log.d("asdf", "has camera");
+        } else {
+            Log.d("Asdf", "no camera2");
+        }
         Log.d("asdf", "testing");
     }
+
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+
 }
