a10f0572ac15528671c58867b5a5f93d7af51861
Soroush Khanlou <soroushkhanlou@gmail.com>
2017-11-05 20:41:22 -0500

    add ability to save photos to the disk

--- app/src/main/java/amal/global/amal/RootActivity.java
+++ app/src/main/java/amal/global/amal/RootActivity.java
@@ -13,10 +13,25 @@ import android.support.v4.app.ActivityCompat;
 import android.util.Size;
 import android.Manifest;
 import android.content.pm.PackageManager;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import android.util.SparseIntArray;
 import android.widget.Toast;
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.media.ImageReader;
+import android.graphics.ImageFormat;
+import android.support.annotation.NonNull;
+import android.media.Image;
+import android.os.Environment;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
 
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
@@ -42,6 +57,14 @@ public class RootActivity extends AppCompatActivity {
     private HandlerThread backgroundThread;
 
     private static final int REQUEST_CAMERA_PERMISSION = 200;
+
+    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0, 90);
+        ORIENTATIONS.append(Surface.ROTATION_90, 0);
+        ORIENTATIONS.append(Surface.ROTATION_180, 270);
+        ORIENTATIONS.append(Surface.ROTATION_270, 180);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -131,7 +154,7 @@ public class RootActivity extends AppCompatActivity {
             captureRequestBuilder.addTarget(surface);
             cameraDevice.createCaptureSession(Arrays.asList(surface), new CameraCaptureSession.StateCallback(){
                 @Override
-                public void onConfigured(CameraCaptureSession lCameraCaptureSession) {
+                public void onConfigured(@NonNull CameraCaptureSession lCameraCaptureSession) {
                     //The camera is already closed
                     if (null == cameraDevice) {
                         return;
@@ -141,7 +164,7 @@ public class RootActivity extends AppCompatActivity {
                     updatePreview();
                 }
                 @Override
-                public void onConfigureFailed(CameraCaptureSession cameraCaptureSession) {
+                public void onConfigureFailed(@NonNull CameraCaptureSession cameraCaptureSession) {
                     Toast.makeText(RootActivity.this, "Configuration change", Toast.LENGTH_SHORT).show();
                 }
             }, null);
@@ -162,7 +185,6 @@ public class RootActivity extends AppCompatActivity {
         }
     }
 
-
     protected void startBackgroundThread() {
         backgroundThread = new HandlerThread("Camera Background");
         backgroundThread.start();
@@ -181,7 +203,93 @@ public class RootActivity extends AppCompatActivity {
     }
 
     protected void takePicture() {
-        Log.d("asdf", "should take picture");
+        if(null == cameraDevice) {
+            Log.e("ASDF", "cameraDevice is null");
+            return;
+        }
+        CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraDevice.getId());
+            Size[] jpegSizes = null;
+            if (characteristics != null) {
+                jpegSizes = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP).getOutputSizes(ImageFormat.JPEG);
+            }
+            int width = 640;
+            int height = 480;
+            if (jpegSizes != null && 0 < jpegSizes.length) {
+                width = jpegSizes[0].getWidth();
+                height = jpegSizes[0].getHeight();
+            }
+            ImageReader reader = ImageReader.newInstance(width, height, ImageFormat.JPEG, 1);
+            List<Surface> outputSurfaces = new ArrayList<Surface>(2);
+            outputSurfaces.add(reader.getSurface());
+            outputSurfaces.add(new Surface(textureView.getSurfaceTexture()));
+            final CaptureRequest.Builder captureBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
+            captureBuilder.addTarget(reader.getSurface());
+            captureBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+            // Orientation
+            int rotation = getWindowManager().getDefaultDisplay().getRotation();
+            captureBuilder.set(CaptureRequest.JPEG_ORIENTATION, ORIENTATIONS.get(rotation));
+            final File dir = new File(getFilesDir() + "/images/");
+            dir.mkdirs();
+            final File file = new File(dir + "/0.jpg");
+            ImageReader.OnImageAvailableListener readerListener = new ImageReader.OnImageAvailableListener() {
+                @Override
+                public void onImageAvailable(ImageReader reader) {
+                    Image image = null;
+                    try {
+                        image = reader.acquireLatestImage();
+                        ByteBuffer buffer = image.getPlanes()[0].getBuffer();
+                        byte[] bytes = new byte[buffer.capacity()];
+                        buffer.get(bytes);
+                        save(bytes);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        if (image != null) {
+                            image.close();
+                        }
+                    }
+                }
+                private void save(byte[] bytes) throws IOException {
+                    OutputStream output = null;
+                    try {
+                        output = new FileOutputStream(file);
+                        output.write(bytes);
+                    } finally {
+                        if (null != output) {
+                            output.close();
+                        }
+                    }
+                }
+            };
+            reader.setOnImageAvailableListener(readerListener, backgroundHandler);
+            final CameraCaptureSession.CaptureCallback captureListener = new CameraCaptureSession.CaptureCallback() {
+                @Override
+                public void onCaptureCompleted(CameraCaptureSession session, CaptureRequest request, TotalCaptureResult result) {
+                    super.onCaptureCompleted(session, request, result);
+                    Toast.makeText(RootActivity.this, "Saved:" + file, Toast.LENGTH_SHORT).show();
+                    createCameraPreview();
+                }
+            };
+            cameraDevice.createCaptureSession(outputSurfaces, new CameraCaptureSession.StateCallback() {
+                @Override
+                public void onConfigured(CameraCaptureSession session) {
+                    try {
+                        session.capture(captureBuilder.build(), captureListener, backgroundHandler);
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+                @Override
+                public void onConfigureFailed(CameraCaptureSession session) {
+                }
+            }, backgroundHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -193,6 +301,14 @@ public class RootActivity extends AppCompatActivity {
             openCamera();
         } else {
             textureView.setSurfaceTextureListener(textureListener);
+        }
+
+
+        File f = getFilesDir();
+        Log.d("asdf", f.getAbsolutePath());
+        File[] files = f.listFiles();
+        for (File file : files) {
+            Log.d("asdf", file.getName());
         }
     }
 
