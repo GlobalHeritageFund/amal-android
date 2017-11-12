01b159ec57355910180ea276b132a1399fc256d9
Soroush Khanlou <soroushkhanlou@gmail.com>
2017-11-12 15:27:57 -0500

    camera should be the first thing you see when you open the app

--- app/src/main/java/amal/global/amal/TabActivity.java
+++ app/src/main/java/amal/global/amal/TabActivity.java
@@ -39,6 +39,7 @@ public class TabActivity extends AppCompatActivity {
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setSelectedItemId(R.id.navigation_capture);
     }
 
 }
