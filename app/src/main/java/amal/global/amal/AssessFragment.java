e990fd8d7753f0dee33317c0d837ac418cd24c7d
Soroush Khanlou <soroushkhanlou@gmail.com>
2017-11-13 16:25:28 -0500

    add basically working assess tab

--- app/src/main/java/amal/global/amal/AssessFragment.java
+++ app/src/main/java/amal/global/amal/AssessFragment.java
@@ -1,55 +1,119 @@
 package amal.global.amal;
 
 import android.content.Context;
-import android.net.Uri;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.util.ArrayList;
 
 
 public class AssessFragment extends Fragment {
-
-    private OnFragmentInteractionListener mListener;
 
     public AssessFragment() {
         // Required empty public constructor
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_assess, container, false);
     }
 
-
-//    @Override
-//    public void onAttach(Context context) {
-//        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
-//    }
-
     @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        GridView gridview = (GridView) getView().findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(getActivity()));
+
+//        gridview.setOnItemClickListener(new OnItemClickListener() {
+//            public void onItemClick(AdapterView<?> parent, View v,
+//                                    int position, long id) {
+//                Toast.makeText(HelloGridView.this, "" + position,
+//                        Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+    }
+}
+
+class ImageAdapter extends BaseAdapter {
+
+    private ArrayList<Image> images;
+
+    private Context context;
+
+    public ImageAdapter(Context c) {
+        context = c;
+        images = fetchImages();
+        Log.d("Asdfasda", String.format("%d", images.size()));
     }
 
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    public ArrayList<Image> fetchImages() {
+        final File dir = new File(context.getFilesDir() + "/images/");
+        dir.mkdirs();
+        File[] files = dir.listFiles();
+        ArrayList<Image> images = new ArrayList<Image>();
+        for (File file : files) {
+            String filename = file.getName();
+            String fileExtension = FilenameUtils.getExtension(filename);
+            String IDString = FilenameUtils.removeExtension(filename);
+            Log.d("asdfasd", fileExtension);
+            if (fileExtension.equals("jpg") || fileExtension.equals("jpeg")) {
+                images.add(new Image(file.getAbsolutePath()));
+            }
+        }
+        return images;
+    }
+
+    @Override
+    public int getCount() {
+        return images.size();
+    }
+
+    public Object getItem(int position) {
+        return images.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(context);
+            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        Image image = (Image)getItem(position);
+        imageView.setImageBitmap(BitmapFactory.decodeFile(image.filePath));
+        return imageView;
+    }
+
+}
+
+class Image {
+    String filePath;
+
+    Image(String filePath) {
+        this.filePath = filePath;
     }
 }
--- app/src/main/java/amal/global/amal/Utils.java
+++ app/src/main/java/amal/global/amal/Utils.java
@@ -20,10 +20,34 @@ class FilenameUtils {
 
         // Remove the extension.
         int extensionIndex = filename.lastIndexOf(".");
-        if (extensionIndex == -1)
+        if (extensionIndex == -1) {
             return filename;
+        }
 
         return filename.substring(0, extensionIndex);
     }
+
+    public static String getExtension(String s) {
+
+        String separator = System.getProperty("file.separator");
+        String filename;
+
+        // Remove the path upto the filename.
+        int lastSeparatorIndex = s.lastIndexOf(separator);
+        if (lastSeparatorIndex == -1) {
+            filename = s;
+        } else {
+            filename = s.substring(lastSeparatorIndex + 1);
+        }
+
+        // Remove the extension.
+        int extensionIndex = filename.lastIndexOf(".");
+        if (extensionIndex == -1) {
+            return filename;
+        }
+
+        return filename.substring(extensionIndex+1);
+    }
+
 
 }
