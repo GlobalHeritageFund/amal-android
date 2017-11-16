80e583ab82bc10f1de35e3441ac7cc4442c9acb7
Soroush Khanlou <soroushkhanlou@gmail.com>
2017-11-16 16:35:51 -0500

    add metadata object

--- app/src/main/java/amal/global/amal/Image.kt
+++ app/src/main/java/amal/global/amal/Image.kt
@@ -1,8 +1,57 @@
 package amal.global.amal
 
-/**
- * Created by soroushkhanlou on 11/14/17.
- */
-class Image internal constructor(public var filePath: String) {
+import org.json.JSONObject
+import java.time.LocalDateTime
 
+data class Image internal constructor(
+        public var filePath: String
+)
+
+data class Metadata internal constructor(
+        public var name: String = "",
+        public var category: String = "",
+        public var levelOfDamage: Int = 0,
+        public var conditionNumber: Int = 0,
+        public var hazards: Boolean = false,
+        public var safetyHazards: Boolean = false,
+        public var interventionRequired: Boolean = false,
+        public var latitude: Double = 0.0,
+        public var longitude: Double = 0.0,
+        public var firebaseImageKey: String = "",
+        public var localIdentifier: String = ""
+) {
+    fun toJSON(): String {
+        var jsonObject = JSONObject()
+        jsonObject.put("name", name)
+        jsonObject.put("category", category)
+        jsonObject.put("levelOfDamage", levelOfDamage)
+        jsonObject.put("conditionNumber", conditionNumber)
+        jsonObject.put("hazards", hazards)
+        jsonObject.put("safetyHazards", safetyHazards)
+        jsonObject.put("interventionRequired", interventionRequired)
+        jsonObject.put("latitude", latitude)
+        jsonObject.put("longitude", longitude)
+        jsonObject.put("firebaseImageKey", firebaseImageKey)
+        jsonObject.put("localIdentifier", localIdentifier)
+        return jsonObject.toString(4)
+    }
+
+    companion object {
+        fun fromJSON(string: String): Metadata {
+            var jsonObject = JSONObject(string)
+            return Metadata(
+                    jsonObject.getString("name"),
+                    jsonObject.getString("category"),
+                    jsonObject.getInt("levelOfDamage"),
+                    jsonObject.getInt("conditionNumber"),
+                    jsonObject.getBoolean("hazards"),
+                    jsonObject.getBoolean("safetyHazards"),
+                    jsonObject.getBoolean("interventionRequired"),
+                    jsonObject.getDouble("latitude"),
+                    jsonObject.getDouble("longitude"),
+                    jsonObject.getString("firebaseImageKey"),
+                    jsonObject.getString("localIdentifier")
+            )
+        }
+    }
 }
