22d614cfc53f2d69bfa3676e95156d98424f711b
Soroush Khanlou <soroushkhanlou@gmail.com>
2017-11-20 00:47:30 -0500

    create an add button for reports

--- app/src/main/java/amal/global/amal/ReportsFragment.kt
+++ app/src/main/java/amal/global/amal/ReportsFragment.kt
@@ -2,16 +2,33 @@ package amal.global.amal
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
+import android.view.*
+
 
 class ReportsFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+        setHasOptionsMenu(true)
         return inflater?.inflate(R.layout.fragment_report, container, false)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_report, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.getItemId()) {
+            R.id.menu_item_add_report -> {
+                return true
+            }
+            else ->
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item)
+        }
+    }
 
 }
