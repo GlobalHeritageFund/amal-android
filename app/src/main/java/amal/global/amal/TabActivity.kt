408ed4cbd736bd1e9e12b28b4415c3099ba90f18
Soroush Khanlou <soroushkhanlou@gmail.com>
2017-11-20 23:58:31 -0500

    simplify tab switching

--- app/src/main/java/amal/global/amal/TabActivity.kt
+++ app/src/main/java/amal/global/amal/TabActivity.kt
@@ -2,29 +2,27 @@ package amal.global.amal
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 
 class TabActivity : AppCompatActivity(), GalleryDelegate {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        var fragment: Fragment? = null
         when (item.itemId) {
             R.id.navigation_assess -> {
-                val assess = GalleryFragment()
-                assess.delegate = this
-                supportFragmentManager.beginTransaction().replace(R.id.content, assess).commit()
-                supportActionBar?.setTitle(R.string.title_assess)
-                return@OnNavigationItemSelectedListener true
+                fragment = GalleryFragment().also { it.delegate = this }
             }
             R.id.navigation_capture -> {
-                supportFragmentManager.beginTransaction().replace(R.id.content, CaptureFragment()).commit()
-                supportActionBar?.setTitle(R.string.title_capture)
-                return@OnNavigationItemSelectedListener true
+                fragment = CaptureFragment()
             }
             R.id.navigation_report -> {
-                supportFragmentManager.beginTransaction().replace(R.id.content, ReportsFragment()).commit()
-                supportActionBar?.setTitle(R.string.title_report)
-                return@OnNavigationItemSelectedListener true
+                fragment = ReportsFragment()
             }
+        }
+        if (fragment != null) {
+            supportFragmentManager.beginTransaction().replace(R.id.content, fragment).commit()
+            return@OnNavigationItemSelectedListener true
         }
         false
     }
