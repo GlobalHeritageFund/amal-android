08ae855555500e5a963947ea09bd865211b86a02
Soroush Khanlou <soroushkhanlou@gmail.com>
2017-11-28 23:17:56 -0500

    use a floating action button for creating a new report

--- app/src/main/java/amal/global/amal/ReportsFragment.kt
+++ app/src/main/java/amal/global/amal/ReportsFragment.kt
@@ -1,8 +1,8 @@
 package amal.global.amal
 
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
 import android.support.v4.app.Fragment
-import android.util.Log
 import android.view.*
 
 interface ReportsDelegate {
@@ -15,31 +15,18 @@ class ReportsFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        setHasOptionsMenu(true)
         return inflater?.inflate(R.layout.fragment_report, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
         activity.setTitle(R.string.title_report)
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
-        inflater.inflate(R.menu.menu_report, menu)
-    }
+        getView()?.findViewById<FloatingActionButton>(R.id.new_report_button)?.setOnClickListener({
+            delegate?.newReportTapped(this)
+        })
 
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        when (item!!.getItemId()) {
-            R.id.menu_item_add_report -> {
-                delegate?.newReportTapped(this)
-                return true
-            }
-            else ->
-                // If we got here, the user's action was not recognized.
-                // Invoke the superclass to handle it.
-                return super.onOptionsItemSelected(item)
-        }
     }
 
 }
