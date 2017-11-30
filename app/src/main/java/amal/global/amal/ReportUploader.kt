ffc2cdbaf8e55ab8bdc1d61be6e8741c467ec78e
Soroush Khanlou <soroushkhanlou@gmail.com>
2017-11-29 22:50:48 -0500

    more scaffolding

--- app/src/main/java/amal/global/amal/ReportUploader.kt
+++ app/src/main/java/amal/global/amal/ReportUploader.kt
@@ -3,8 +3,21 @@ package amal.global.amal
 class ReportUploader (val reportDraft: ReportDraft) {
 
     val promise = Promise<Report>()
-    
-    fun upload() {
 
+    fun upload() {
+        val firebase = Firebase()
+        Promise.all<Unit>(sequenceOf<Promise<Unit>>(
+                firebase.set("title", reportDraft.title),
+                firebase.set("authorDeviceToken", reportDraft.localIdentifier),
+                firebase.set("creationDate", reportDraft.creationDate.time)
+        ))
+    }
+}
+
+class Firebase {
+    fun set(key: String, value: Any): Promise<Unit> {
+        return Promise<Unit>({ fulfill, reject ->
+            reject(Error("placeholder"))
+        })
     }
 }
