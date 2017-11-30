2ae2efba26a603513739a6ed6953dc81ccec98b7
Soroush Khanlou <soroushkhanlou@gmail.com>
2017-11-29 22:31:49 -0500

    update new report form with some logic

--- app/src/main/java/amal/global/amal/NewReportFragment.kt
+++ app/src/main/java/amal/global/amal/NewReportFragment.kt
@@ -2,15 +2,56 @@ package amal.global.amal
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.EditText
+import android.widget.TextView
+
+interface NewReportFragmentDelegate {
+    fun uploadReport(fragment: NewReportFragment, report: ReportDraft)
+}
+
+class ReportDraft { }
 
 class NewReportFragment: Fragment() {
+
+    lateinit var titleField: EditText
+    lateinit var dateLabel: TextView
+    lateinit var emailField: EditText
+
+    var delegate: NewReportFragmentDelegate? = null
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         setHasOptionsMenu(true)
         return inflater?.inflate(R.layout.fragment_new_report, container, false)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_new_report, menu)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        activity.setTitle(R.string.title_report)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        titleField = bind(R.id.report_title_field)
+        dateLabel = bind(R.id.report_creation_date)
+        emailField = bind(R.id.report_assessor_email)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.getItemId()) {
+            R.id.menu_item_upload_report -> {
+                delegate?.uploadReport(this, ReportDraft())
+                return true
+            }
+            else ->
+                return super.onOptionsItemSelected(item)
+        }
+    }
 }
