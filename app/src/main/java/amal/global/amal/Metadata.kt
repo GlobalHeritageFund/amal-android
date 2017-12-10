8455c0cc6c497235e513d579e0af17f607e5fa5a
Soroush Khanlou <soroushkhanlou@gmail.com>
2017-12-09 21:15:40 -0500

    extract Metadata into its own file

---
+++ app/src/main/java/amal/global/amal/Metadata.kt
@@ -0,0 +1,53 @@
+package amal.global.amal
+
+import org.json.JSONObject
+
+
+data class Metadata internal constructor(
+        public var name: String = "",
+        public var category: String = "",
+        public var levelOfDamage: Int = 0,
+        public var conditionNumber: Int = 0,
+        public var hazards: Boolean = false,
+        public var safetyHazards: Boolean = false,
+        public var interventionRequired: Boolean = false,
+        public var latitude: Double = 0.0,
+        public var longitude: Double = 0.0,
+        public var firebaseImageKey: String = "",
+        public var localIdentifier: String = ""
+) {
+    fun toJSON(): String {
+        val jsonObject = JSONObject()
+        jsonObject.put("name", name)
+        jsonObject.put("category", category)
+        jsonObject.put("levelOfDamage", levelOfDamage)
+        jsonObject.put("conditionNumber", conditionNumber)
+        jsonObject.put("hazards", hazards)
+        jsonObject.put("safetyHazards", safetyHazards)
+        jsonObject.put("interventionRequired", interventionRequired)
+        jsonObject.put("latitude", latitude)
+        jsonObject.put("longitude", longitude)
+        jsonObject.put("firebaseImageKey", firebaseImageKey)
+        jsonObject.put("localIdentifier", localIdentifier)
+        return jsonObject.toString(4)
+    }
+
+    companion object {
+        fun fromJSON(string: String): Metadata {
+            val jsonObject = JSONObject(string)
+            return Metadata(
+                    jsonObject.getString("name"),
+                    jsonObject.getString("category"),
+                    jsonObject.getInt("levelOfDamage"),
+                    jsonObject.getInt("conditionNumber"),
+                    jsonObject.getBoolean("hazards"),
+                    jsonObject.getBoolean("safetyHazards"),
+                    jsonObject.getBoolean("interventionRequired"),
+                    jsonObject.getDouble("latitude"),
+                    jsonObject.getDouble("longitude"),
+                    jsonObject.getString("firebaseImageKey"),
+                    jsonObject.getString("localIdentifier")
+            )
+        }
+    }
+}
