5e5447dde9f974c093a779f4fd16c3b220df5dc9
Soroush Khanlou <soroushkhanlou@gmail.com>
2017-12-17 19:14:54 -0500

    add more to onboarding

--- app/src/main/java/amal/global/amal/OnboardingActivity.kt
+++ app/src/main/java/amal/global/amal/OnboardingActivity.kt
@@ -2,15 +2,24 @@ package amal.global.amal
 
 import android.support.v7.app.AppCompatActivity
 import android.content.Intent
-import android.R.id.edit
-import android.content.SharedPreferences
-
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_onboarding.*
 
 
 class OnboardingActivity : AppCompatActivity() {
 
     val currentUser: CurrentUser by lazy {
         CurrentUser(this)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+
+        this.skipButton.setOnClickListener({ view ->
+            finishOnboarding()
+        })
+
     }
 
     private fun finishOnboarding() {
