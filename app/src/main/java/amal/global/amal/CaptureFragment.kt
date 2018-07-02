378b5b4b527abd8a1b517a82c0b2b160bcca84fa
Soroush Khanlou <soroushkhanlou@gmail.com>
2018-07-02 16:39:06 -0400

    switch to camera kit for capture

--- app/src/main/java/amal/global/amal/CaptureFragment.kt
+++ app/src/main/java/amal/global/amal/CaptureFragment.kt
@@ -3,38 +3,17 @@ package amal.global.amal
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
-import android.graphics.*
-import android.hardware.camera2.CameraAccessException
-import android.hardware.camera2.CameraCaptureSession
-import android.hardware.camera2.CameraCharacteristics
-import android.hardware.camera2.CameraDevice
-import android.hardware.camera2.CameraManager
-import android.hardware.camera2.CameraMetadata
-import android.hardware.camera2.CaptureRequest
-import android.hardware.camera2.TotalCaptureResult
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
-import android.media.Image
-import android.media.ImageReader
 import android.os.Bundle
-import android.os.Handler
-import android.os.HandlerThread
-import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
-import android.util.Log
-import android.util.Size
 import android.view.LayoutInflater
-import android.view.Surface
-import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.Toast
+import com.wonderkiln.camerakit.*
 import kotlinx.android.synthetic.main.fragment_capture.*
 
-import java.io.FileNotFoundException
-import java.io.IOException
 
 interface CaptureDelegate {
     fun settingsButtonTapped(fragment: CaptureFragment)
@@ -42,70 +21,9 @@ interface CaptureDelegate {
 
 class CaptureFragment : Fragment() {
 
-    private var imageDimension = Size(640, 480)
-    private var cameraDevice: CameraDevice? = null
-    private var backgroundHandler: Handler? = null
-    private var backgroundThread: HandlerThread? = null
     private var lastLocation: Location? = null
 
     var delegate: CaptureDelegate? = null
-
-    private val cameraManager by lazy {
-        activity!!.getSystemService(Context.CAMERA_SERVICE) as CameraManager
-    }
-
-    private var textureListener: TextureView.SurfaceTextureListener = object : TextureView.SurfaceTextureListener {
-        override fun onSurfaceTextureAvailable(surface: SurfaceTexture, width: Int, height: Int) {
-            openCamera()
-            transformTexture(width, height)
-        }
-
-        override fun onSurfaceTextureSizeChanged(surface: SurfaceTexture, width: Int, height: Int) { }
-
-        override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean {
-            return false
-        }
-
-        override fun onSurfaceTextureUpdated(surface: SurfaceTexture) { }
-    }
-
-    private val stateCallback = object : CameraDevice.StateCallback() {
-        override fun onOpened(camera: CameraDevice) {
-            cameraDevice = camera
-            createCameraPreview()
-        }
-
-        override fun onDisconnected(camera: CameraDevice) {
-            closeCamera()
-        }
-
-        override fun onError(camera: CameraDevice, error: Int) {
-            closeCamera()
-        }
-    }
-
-    fun transformTexture(width: Int, height: Int) {
-        val matrix = Matrix()
-
-        val rotation = activity!!.windowManager.defaultDisplay.rotation
-        if (rotation == Surface.ROTATION_90 || rotation == Surface.ROTATION_270) {
-
-            val textureRectF = RectF(0.0f, 0.0f, width.toFloat(), height.toFloat())
-            val previewRectF = RectF(0.0f, 0.0f, textureView.height.toFloat(), textureView.width.toFloat())
-
-            val centerX = textureRectF.centerX()
-            val centerY = textureRectF.centerY()
-
-            previewRectF.offset(centerX - previewRectF.centerX(), centerY - previewRectF.centerY());
-
-            matrix.setRectToRect(textureRectF, previewRectF, Matrix.ScaleToFit.START);
-
-            val scale = Math.max(width.toFloat() / width, height.toFloat() / width);
-            matrix.postScale(scale, scale, centerX, centerY);
-            matrix.postRotate(90.toFloat() * (rotation - 2), centerX, centerY);
-        }
-        textureView.setTransform(matrix)
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -114,7 +32,6 @@ class CaptureFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        textureView.surfaceTextureListener = textureListener
         shutterButton.setOnClickListener({ takePicture() })
         settingsButton.setOnClickListener({ delegate?.settingsButtonTapped(this) })
     }
@@ -122,58 +39,6 @@ class CaptureFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         activity!!.setTitle(R.string.title_capture)
-    }
-
-    fun openCamera() {
-        try {
-            val cameraId = cameraManager.cameraIdList.first()
-            imageDimension = cameraManager
-                    .getCameraCharacteristics(cameraId)
-                    .get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
-                    .getOutputSizes(SurfaceTexture::class.java)
-                    .first()
-
-            val permissions = arrayOf(
-                    Manifest.permission.CAMERA,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                    Manifest.permission.ACCESS_FINE_LOCATION
-            )
-            if (permissions.all({ ActivityCompat.checkSelfPermission(activity!!, it) != PackageManager.PERMISSION_GRANTED })) {
-                ActivityCompat.requestPermissions(activity!!, permissions, requestCameraPermission)
-                return
-            }
-            cameraManager.openCamera(cameraId, stateCallback, null)
-        } catch (e: CameraAccessException) {
-            e.printStackTrace()
-        }
-    }
-
-    fun createCameraPreview() {
-        try {
-            val texture = textureView.surfaceTexture
-            texture.setDefaultBufferSize(imageDimension.width, imageDimension.height)
-            val surface = Surface(texture)
-            val captureRequestBuilder = cameraDevice?.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
-            captureRequestBuilder?.addTarget(surface)
-            cameraDevice?.createCaptureSession(listOf(surface), object : CameraCaptureSession.StateCallback() {
-                override fun onConfigured(cameraCaptureSession: CameraCaptureSession) {
-                    if (cameraDevice != null) {
-                        captureRequestBuilder?.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO)
-                        try {
-                            cameraCaptureSession.setRepeatingRequest(captureRequestBuilder!!.build(), null, backgroundHandler)
-                        } catch (e: CameraAccessException) {
-                            e.printStackTrace()
-                        }
-                    }
-                }
-
-                override fun onConfigureFailed(cameraCaptureSession: CameraCaptureSession) {
-                    Toast.makeText(activity, "Configuration change", Toast.LENGTH_SHORT).show()
-                }
-            }, null)
-        } catch (e: CameraAccessException) {
-            e.printStackTrace()
-        }
     }
 
     private fun beginListeningForLocation() {
@@ -195,131 +60,37 @@ class CaptureFragment : Fragment() {
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
             lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
         }
-
-    }
-
-    private fun startBackgroundThread() {
-        backgroundThread = HandlerThread("Camera Background")
-        backgroundThread?.start()
-        backgroundHandler = Handler(backgroundThread!!.looper)
-    }
-
-    private fun stopBackgroundThread() {
-        backgroundThread?.quitSafely()
-        try {
-            backgroundThread?.join()
-            backgroundThread = null
-            backgroundHandler = null
-        } catch (e: InterruptedException) {
-            e.printStackTrace()
-        }
     }
 
     private fun takePicture() {
-        if (cameraDevice == null) {
-            Log.e("ASDF", "trying to capture a picture without a cameraDevice")
-            return
-        }
-        try {
-            val jpegSizes = cameraManager
-                    .getCameraCharacteristics(cameraDevice!!.id)
-                    .get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
-                    .getOutputSizes(ImageFormat.JPEG)
+        cameraView.addCameraKitListener(object : CameraKitEventListener {
+            override fun onEvent(event: CameraKitEvent) { }
 
-            val width = jpegSizes?.first()?.width ?: 640
-            val height = jpegSizes?.first()?.height ?: 480
+            override fun onError(error: CameraKitError) { }
 
-            val reader = ImageReader.newInstance(width, height, ImageFormat.JPEG, 1)
-            val outputSurfaces = listOf<Surface>(reader.surface, Surface(textureView.surfaceTexture))
+            override fun onImage(image: CameraKitImage) {
 
-            val captureBuilder = cameraDevice!!.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE)
-            captureBuilder.addTarget(reader.surface)
-            captureBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO)
-            captureBuilder.set(CaptureRequest.JPEG_ORIENTATION, orientationFor(activity!!.windowManager.defaultDisplay.rotation))
+                val metadata = Metadata()
+                metadata.latitude = lastLocation?.latitude ?: 0.0
+                metadata.longitude = lastLocation?.longitude ?: 0.0
 
-            val metadata = Metadata()
-            metadata.latitude = lastLocation?.latitude ?: 0.0
-            metadata.longitude = lastLocation?.longitude ?: 0.0
-
-            val readerListener = ImageReader.OnImageAvailableListener { reader ->
-                var image: Image? = null
-                try {
-                    image = reader.acquireLatestImage()
-                    val buffer = image?.planes?.firstOrNull()?.buffer
-                    val bytes = ByteArray(buffer!!.capacity())
-                    buffer.get(bytes)
-                    PhotoStorage(activity!!).savePhotoLocally(bytes, metadata)
-                } catch (e: FileNotFoundException) {
-                    e.printStackTrace()
-                } catch (e: IOException) {
-                    e.printStackTrace()
-                } finally {
-                    image?.close()
-                }
+                PhotoStorage(activity!!).savePhotoLocally(image.jpeg, metadata)
             }
-            reader.setOnImageAvailableListener(readerListener, backgroundHandler)
-            val captureListener = object : CameraCaptureSession.CaptureCallback() {
-                override fun onCaptureCompleted(session: CameraCaptureSession, request: CaptureRequest, result: TotalCaptureResult) {
-                    super.onCaptureCompleted(session, request, result)
-                    createCameraPreview()
-                }
-            }
-            cameraDevice!!.createCaptureSession(outputSurfaces, object : CameraCaptureSession.StateCallback() {
-                override fun onConfigured(session: CameraCaptureSession) {
-                    try {
-                        session.capture(captureBuilder.build(), captureListener, backgroundHandler)
-                    } catch (e: CameraAccessException) {
-                        e.printStackTrace()
-                    }
-                }
 
-                override fun onConfigureFailed(session: CameraCaptureSession) {}
-            }, backgroundHandler)
-        } catch (e: CameraAccessException) {
-            e.printStackTrace()
-        }
+            override fun onVideo(video: CameraKitVideo) { }
+        })
+        cameraView.captureImage()
     }
 
     override fun onResume() {
         super.onResume()
+        cameraView.start()
         beginListeningForLocation()
-        startBackgroundThread()
-        if (textureView.isAvailable) {
-            openCamera()
-        } else {
-            textureView.surfaceTextureListener = textureListener
-        }
     }
 
     override fun onPause() {
-        closeCamera();
-        stopBackgroundThread()
+        cameraView.stop()
         super.onPause()
     }
 
-    private fun closeCamera() {
-        cameraDevice?.close()
-        cameraDevice = null
-    }
-
-    private val requestCameraPermission = 200
-
-    private fun orientationFor(rotation: Int): Int {
-        when (rotation) {
-            Surface.ROTATION_0 -> {
-                return 90
-            }
-            Surface.ROTATION_90 -> {
-                return 0
-            }
-            Surface.ROTATION_180 -> {
-                return 270
-            }
-            Surface.ROTATION_270 -> {
-                return 170
-            }
-        }
-        return 0
-    }
 }
-
