35ba13464fdf3d0a4b9c904e38a0f569fb3869a8
Soroush Khanlou <soroushkhanlou@gmail.com>
2018-07-04 21:23:56 -0400

    clean up ReportsAdapter

--- app/src/main/java/amal/global/amal/ReportsAdapter.kt
+++ app/src/main/java/amal/global/amal/ReportsAdapter.kt
@@ -4,18 +4,14 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.support.v7.widget.RecyclerView
 import android.util.Log
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import java.util.concurrent.Semaphore
 
-class ReportsAdapter(context: Context, var reports: List<Report> = listOf()) : RecyclerView.Adapter<SubtitleViewHolder>() {
-
+class ReportsAdapter(var context: Context, var reports: List<Report> = listOf()) : RecyclerView.Adapter<SubtitleViewHolder>() {
 
     val semaphore = Semaphore(3)
 
