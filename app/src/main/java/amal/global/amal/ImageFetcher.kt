6b5a0cebd498c9aad04a31d77f1a2ea35f22c5e7
Soroush Khanlou <soroushkhanlou@gmail.com>
2018-07-07 20:20:58 -0400

    add an image cache to the ImageFetcher

--- app/src/main/java/amal/global/amal/ImageFetcher.kt
+++ app/src/main/java/amal/global/amal/ImageFetcher.kt
@@ -1,17 +1,44 @@
 package amal.global.amal
 
 import android.graphics.Bitmap
-import com.google.firebase.storage.FirebaseStorage
+import android.util.LruCache
 import com.google.firebase.storage.StorageReference
 
 class ImageFetcher() {
 
+    val cache = ImageMemoryCache
     fun fetchImage(firebaseReference: StorageReference): Promise<Bitmap> {
+        cache.get(firebaseReference.toString())?.let {
+            return@fetchImage Promise(it)
+        }
         return firebaseReference
                 .getBytesPromise(Long.MAX_VALUE)
                 .flatMap { byteArray ->
                     byteArray.decodeBitmap()
                 }
+                .then { bitmap ->
+                    cache.set(firebaseReference.toString(), bitmap)
+                }
+    }
+}
 
+object ImageMemoryCache {
+
+    private val maxMemory = Runtime.getRuntime().maxMemory().toInt()
+
+    private val cacheSize = maxMemory / 8
+
+    private val cache = object: LruCache<String, Bitmap>(cacheSize) {
+        override fun sizeOf(key: String?, bitmap: Bitmap?): Int {
+            return bitmap?.let { it.rowBytes * it.height } ?: 0
+        }
+    }
+
+    fun get(key: String): Bitmap? {
+        return cache.get(key)
+    }
+
+    fun set(key: String, bitmap: Bitmap) {
+        cache.put(key, bitmap)
     }
 }
