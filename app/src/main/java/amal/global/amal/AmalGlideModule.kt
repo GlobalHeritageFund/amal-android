f78b9b2af95eb16756b2c05285ed48d749130263
Soroush Khanlou <soroushkhanlou@gmail.com>
2018-07-10 19:40:38 -0400

    add glide

---
+++ app/src/main/java/amal/global/amal/AmalGlideModule.kt
@@ -0,0 +1,7 @@
+package amal.global.amal
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class AppGlideModule : AppGlideModule()
