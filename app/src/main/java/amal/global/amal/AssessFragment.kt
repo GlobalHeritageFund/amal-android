39f328b833d5469f62492577f9a6fd77fe6ec123
Soroush Khanlou <soroushkhanlou@gmail.com>
2018-07-13 14:00:59 -0400

    use kotlin synthetic property binding for subviews

--- app/src/main/java/amal/global/amal/AssessFragment.kt
+++ app/src/main/java/amal/global/amal/AssessFragment.kt
@@ -6,7 +6,6 @@ import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.model.CameraPosition
@@ -19,15 +18,6 @@ class AssessFragment : Fragment() {
     var image: LocalImage? = null
 
     lateinit var mapView: MapView
-    lateinit var imageView: ImageView
-    lateinit var nameField: EditText
-    lateinit var categoryRadioGroup: RadioGroup
-    lateinit var conditionRadioGroup: RadioGroup
-    lateinit var hazardsCheckBox: CheckBox
-    lateinit var safetyHazardsCheckBox: CheckBox
-    lateinit var interventionCheckBox: CheckBox
-    lateinit var notesField: EditText
-    lateinit var coordinatesTextView: TextView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -39,10 +29,7 @@ class AssessFragment : Fragment() {
 
         explicitlyBindMapViewSoItDoesntGetDeallocatedForOnDestroy()
 
-        imageView = bind(R.id.image_view)
 
-        nameField = bind(R.id.name_field)
-        nameField.setText(image?.metadata?.name ?: "")
         nameField.afterTextChanged { editable: Editable? ->
             image?.metadata?.name = editable.toString()
             image?.saveMetaData()
@@ -67,59 +54,54 @@ class AssessFragment : Fragment() {
             }
         })
 
-        categoryRadioGroup = bind(R.id.category_radio_group)
         when (image?.metadata?.category ?: "") {
-            "area" -> categoryRadioGroup.check(R.id.radio_overall)
-            "site" -> categoryRadioGroup.check(R.id.radio_site)
-            "object" -> categoryRadioGroup.check(R.id.radio_object)
+            "area" -> categoryRadioGroup.check(R.id.radioOverall)
+            "site" -> categoryRadioGroup.check(R.id.radioSite)
+            "object" -> categoryRadioGroup.check(R.id.radioObject)
         }
         categoryRadioGroup.setOnCheckedChangeListener({ radioGroup, checkedID ->
             val string = when (checkedID) {
-                R.id.radio_overall -> "area"
-                R.id.radio_site -> "site"
-                R.id.radio_object -> "object"
+                R.id.radioOverall -> "area"
+                R.id.radioSite -> "site"
+                R.id.radioObject -> "object"
                 else -> ""
             }
             image?.metadata?.category = string
             image?.saveMetaData()
         })
 
-        conditionRadioGroup = bind(R.id.condition_radio_group)
         when (image?.metadata?.conditionNumber ?: 0) {
-            1 -> conditionRadioGroup.check(R.id.radio_condition_1)
-            2 -> conditionRadioGroup.check(R.id.radio_condition_2)
-            3 -> conditionRadioGroup.check(R.id.radio_condition_3)
-            4 -> conditionRadioGroup.check(R.id.radio_condition_4)
-            5 -> conditionRadioGroup.check(R.id.radio_condition_5)
+            1 -> conditionRadioGroup.check(R.id.radioCondition1)
+            2 -> conditionRadioGroup.check(R.id.radioCondition2)
+            3 -> conditionRadioGroup.check(R.id.radioCondition3)
+            4 -> conditionRadioGroup.check(R.id.radioCondition4)
+            5 -> conditionRadioGroup.check(R.id.radioCondition5)
         }
         conditionRadioGroup.setOnCheckedChangeListener({ radioGroup, checkedID ->
             val value = when (checkedID) {
-                R.id.radio_condition_1 -> 1
-                R.id.radio_condition_2 -> 2
-                R.id.radio_condition_3 -> 3
-                R.id.radio_condition_4 -> 4
-                R.id.radio_condition_5 -> 5
+                R.id.radioCondition1 -> 1
+                R.id.radioCondition2 -> 2
+                R.id.radioCondition3 -> 3
+                R.id.radioCondition4 -> 4
+                R.id.radioCondition5 -> 5
                 else -> 0
             }
             image?.metadata?.conditionNumber = value
             image?.saveMetaData()
         })
 
-        hazardsCheckBox = bind(R.id.checkbox_hazards)
         hazardsCheckBox.isChecked = image?.metadata?.hazards ?: false
         hazardsCheckBox.setOnCheckedChangeListener({ buttonView, isChecked ->
             image?.metadata?.hazards = isChecked
             image?.saveMetaData()
         })
 
-        safetyHazardsCheckBox = bind(R.id.checkbox_safety)
         safetyHazardsCheckBox.isChecked = image?.metadata?.safetyHazards ?: false
         safetyHazardsCheckBox.setOnCheckedChangeListener({ buttonView, isChecked ->
             image?.metadata?.safetyHazards = isChecked
             image?.saveMetaData()
         })
 
-        interventionCheckBox = bind(R.id.checkbox_intervention)
         interventionCheckBox.isChecked = image?.metadata?.interventionRequired ?: false
         interventionCheckBox.setOnCheckedChangeListener({ buttonView, isChecked ->
             image?.metadata?.interventionRequired = isChecked
@@ -127,14 +109,12 @@ class AssessFragment : Fragment() {
         })
 
 
-        notesField = bind(R.id.notes)
         notesField.setText(image?.metadata?.notes ?: "")
         notesField.afterTextChanged { editable: Editable? ->
             image?.metadata?.notes = editable.toString()
             image?.saveMetaData()
         }
 
-        coordinatesTextView = bind(R.id.coordinates_text_view)
         coordinatesTextView.text = image?.metadata?.coordinatesString()
 
         updateImage()
