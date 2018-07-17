58a2c931fffc63d3443fe1f1f3fe06d4426bfcb8
Soroush Khanlou <soroushkhanlou@gmail.com>
2018-07-17 16:42:19 -0400

    add Parser.fetchOptional

--- app/src/main/java/amal/global/amal/Parser.kt
+++ app/src/main/java/amal/global/amal/Parser.kt
@@ -5,10 +5,19 @@ import java.security.InvalidParameterException
 class Parser(val map: HashMap<String, Any>) {
 
     fun <T> fetch(key: String): T {
-        val untyped = map.get(key) ?: throw InvalidParameterException("Value at $key not found.")
+        val untyped = map[key] ?: throw InvalidParameterException("Value at $key not found.")
 
         val typed = (untyped as? T) ?: throw InvalidParameterException("Value at key $key was type ${key.javaClass} but should have been something else.")
 
         return typed
+    }
+
+    fun <T> fetchOptional(key: String): T? {
+        val untyped = map[key] ?: return null
+
+        val typed = (untyped as? T) ?: throw InvalidParameterException("Value at key $key was type ${key.javaClass} but should have been something else.")
+
+        return typed
+
     }
 }
