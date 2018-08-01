3412e90211d1c6ca72d59cb81053c3c7e11494ca
Soroush Khanlou <soroushkhanlou@gmail.com>
2018-08-01 16:23:31 -0400

    add account section and the ability to sign out

--- app/src/main/java/amal/global/amal/SettingsFragment.kt
+++ app/src/main/java/amal/global/amal/SettingsFragment.kt
@@ -2,19 +2,39 @@ package amal.global.amal
 
 import android.os.Bundle
 import android.support.v7.preference.PreferenceFragmentCompat
-import android.view.View
 import android.content.pm.PackageManager
-import android.R.attr.versionName
-import android.content.pm.PackageInfo
-
+import android.support.v7.preference.Preference
 
 
 internal class SettingsFragment: PreferenceFragmentCompat() {
 
+    private val currentUser: CurrentUser
+        get() = CurrentUser(this.context!!)
+
+
+    private val versionPreference: Preference
+        get() = findPreference("versionPreference")
+
+    private val authPreference: Preference
+        get() = findPreference("auth")
+
     override fun onCreatePreferences(bundle: Bundle?, string: String?) {
         addPreferencesFromResource(R.xml.preferences)
 
-        val versionPreference = findPreference("versionPreference")
+        configureView()
+
+        authPreference.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+            if (currentUser.isLoggedIn) {
+                currentUser.signOut()
+            } else {
+                //call delegate and sign in
+            }
+            configureView()
+            true
+        }
+    }
+
+    fun configureView() {
 
         try {
             val pInfo = activity!!.packageManager.getPackageInfo(activity!!.packageName, 0)
@@ -24,6 +44,7 @@ internal class SettingsFragment: PreferenceFragmentCompat() {
             e.printStackTrace()
         }
 
+        authPreference.title = if (currentUser.isLoggedIn ) "Sign Out" else "Sign In"
 
     }
 }
